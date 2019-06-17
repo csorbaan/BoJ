9fcfdcb45041632d36b007dbb950355f268995d1
55yoghurt55 <38226708+55yoghurt55@users.noreply.github.com>
2019-06-17 22:02:42 +0200

    MenüUpg
    Menü, zene, stb...

---
+++ BoJ V2/Assets/InGameMenü.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+
+public class InGameMenü : MonoBehaviour
+{
+    public GameObject inGameMenu;
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            inGameMenu.SetActive(true);
+    }
+
+    public void BackToGame()
+    {
+        inGameMenu.SetActive(false);
+    }
+
+    public void Exit()
+    {
+        SceneManager.LoadScene("Menü");
+    }
+}
---
+++ BoJ V2/Assets/Scripts/Menü.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menü : MonoBehaviour
+{
+    public void continueGame()
+    {
+        SceneManager.LoadScene("Teszt");
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
