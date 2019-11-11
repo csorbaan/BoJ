0aeff535c86e8fc16f06625a8d35fa20185ef9c7
csorbaan <csorbaan@users.noreply.github.com>
2019-11-11 20:00:52 +0100

    sa

---
+++ BoJ V2/Assets/Scripts/EqpSlot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class EqpSlot : MonoBehaviour
+{
+    public GameObject item;
+    public Transform slotIconGO;
+    public bool empty;
+    public int ID;
+    public string type;
+    public string itemName;
+    public Sprite icon;
+
+    private void Start()
+    {
+        slotIconGO = transform.GetChild(0);
+    }
+
+    public void UpdateSlot()
+    {
+        slotIconGO.GetComponent<Image>().sprite = icon;
+    }
+}
